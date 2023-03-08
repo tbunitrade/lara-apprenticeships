2961950257497876eabb2208eb0a9746d0a8ac69
osony <osony@softserveinc.com>
2023-03-08 17:07:43 +0200

    fix timezone

--- advokatapp/config/app.php
+++ advokatapp/config/app.php
@@ -69,7 +69,8 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    // 'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => ('Europe/Kyiv'),
 
     /*
     |--------------------------------------------------------------------------
